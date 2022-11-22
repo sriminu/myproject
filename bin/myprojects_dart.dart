10467a91e63aa483b66fe8b7c04974a0a9ec6736
Lenovo <sriminu1998@gmail.com>
2022-11-22 15:21:21 +0530

    commit

---
+++ bin/myprojects_dart.dart
@@ -0,0 +1,5 @@
+import 'package:myprojects_dart/myprojects_dart.dart' as myprojects_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${myprojects_dart.calculate()}!');
+}
---
+++ test/myprojects_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:myprojects_dart/myprojects_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
